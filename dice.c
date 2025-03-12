774ec6ccd74d5e927ef6c04072d6b08471e3410a
c-D-a-d-a-m-s <christopheradams1022@gmail.com>
2025-03-11 22:48:24 -0500

    Add basic roll mechanic
    Running program simulates rolling a single d6 die.

--- dice.c
+++ dice.c
@@ -9,10 +9,24 @@
  *************************************************************/
 
  /*** includes ***/
+ #include <stdlib.h>
+ #include <stdio.h>
+ #include <time.h>
+
+ /*** constants ***/
+ const int D6 = 6;
+
+ /*** mechanics ***/
+ int roll_die(int sides) {
+    return rand() % sides + 1;
+ }
 
 /*** init ***/
 int main() {
-
+    srand(time(NULL));
+    int result;
+    result = roll_die(D6);
+    printf("d6: %d\n\n", result);
 
     return 0;
 }
