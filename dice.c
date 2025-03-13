72b54e570ae74f3758c3407fedca70bb4066c19d
c-D-a-d-a-m-s <christopheradams1022@gmail.com>
2025-03-12 20:45:04 -0500

    Add user-capability to specify number of dice
    User can now specify how many dice of each user-specified dice type to
    roll through command line arguments.

--- dice.c
+++ dice.c
@@ -15,7 +15,14 @@
  #include <regex.h>
 
  /*** user-defined data type ***/
- typedef struct {int sides;} RollType;
+ typedef struct {int sides;
+                int count;} RollType;
+
+/*** utilities ***/
+void die() {
+    printf("ERROR: Invalid command line argument passed into program.\n");
+    exit(-1);
+}
 
  /*** mechanics ***/
  int roll_die(int sides) {
@@ -26,21 +33,48 @@
 int main(int argc, char** argv) {
     srand(time(NULL));
     regex_t rollRegex;
-    regcomp(&rollRegex, "^[0,1,2,3,4,5,6,7,8,9]*$", REG_NOSUB);
+    regcomp(&rollRegex, "^[c,d][0,1,2,3,4,5,6,7,8,9]\\{1,3\\}$", REG_NOSUB);
+
+    /*Parse command line arguments*/
+    const int USER_ROLLS_MAX = 20;
+    RollType userRolls[USER_ROLLS_MAX];
+    int userRollsTail = -1;
 
     for (int i = 1; i < argc; i++) {
         if (regexec(&rollRegex, *(argv + i), 0, NULL, 0) != 0) {
-            printf("ERROR: Invalid command line argument passed into program.\n");
-            exit(-1);
+            die();
+        }
+        if (i == 1 && **(argv + i) != 'd') {
+            die();
+        }
+        if (**(argv + i) == 'd') {
+            userRollsTail++;
+            if (userRollsTail == USER_ROLLS_MAX) {
+                printf("Maximum number of dice types exceeded.\n");
+                userRollsTail--;
+                break;
+            }
+            userRolls[userRollsTail].sides = atoi(*(argv + i) + 1);
+            userRolls[userRollsTail].count = 1; //default count
+        }
+        if (**(argv + i) == 'c' && **(argv + i - 1) != 'd') {
+            die();
+        }
+        if (**(argv + i) == 'c' && **(argv + i - 1) == 'd') {
+            userRolls[userRollsTail].count = atoi(*(argv + i) + 1);
         }
     }
 
-    for (int i = 1; i < argc; i++) {
-        int sides = atoi(*(argv + i));
-        int result;
+    int result;
 
-        result = roll_die(sides);
-        printf("d%d: %d\n\n", sides, result);
+    /*roll dice and display results*/
+    for (int i = 0; i <= userRollsTail; i++) {
+        printf("d%d: ", userRolls[i].sides);
+        for (int j = 0; j < userRolls[i].count; j++) {
+            result = roll_die(userRolls[i].sides);
+            printf("[%d]", result);
+        }
+        printf("\n");
     }
 
 
